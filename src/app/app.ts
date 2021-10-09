98a3bc724139fb411494dcf2643fe6b2b3a674d7
Chris Langenberg <speedmaze@gmail.com>
2021-10-09 23:42:25 +0200

    Refactor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { JitsiBridge } from "./jitsi-bridge";
 import { ObsBridge } from "./obs-bridge";
 
-const obs = new ObsBridge()
-const jitsiBridge = new JitsiBridge(obs);
+function main() {
+  const obs = new ObsBridge();
+  const jitsiBridge = new JitsiBridge(obs);
 
-(() => {
-    setTimeout(() => {
-        jitsiBridge.setup()
-    }, 5000)
-})()
+  setTimeout(() => {
+    jitsiBridge.setup();
+  }, 5000);
+}
+
+main()
